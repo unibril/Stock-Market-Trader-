c66c8f00a7de9978c8fd15880f012ad64a6a7ca6
Unibril Karki <unibrilkarki@gmail.com>
2026-04-05 22:18:30 +0545

    now supports upgraded sell function

--- profit_loss_calcu.py
+++ profit_loss_calcu.py
@@ -1,17 +1,21 @@
 from history_for_profit_loss import history_for_profit_loss
-def profit_or_loss(entry_price, exit_price, quantity,stock,user_id):
-    final_amt = round(exit_price - entry_price, 2)
-    total = round(final_amt * quantity, 2)
-    
+
+def profit_or_loss(sold_batches, exit_price, stock, user_id):
+    total = 0
+
+    for buy_price, quantity in sold_batches:
+        total += round((exit_price - buy_price) * quantity, 2)
+    total = round(total, 2)
+
     if total > 0:
-        print(f"Profit: ${final_amt} per share, ${total} total for {quantity} shares.")
+        print(f"Total Profit: ${total}")
     elif total < 0:
-        print(f"Loss: ${abs(final_amt)} per share, ${abs(total)} total for {quantity} shares.")
+        print(f"Total Loss: ${abs(total)}")
     else:
         print("You broke even on this trade.")
-    
-    sure = input("Do you want histroy of profit or loss? ")
+
+    sure = input("Do you want history of profit or loss? ")
     if sure.lower() == "yes":
-        history_for_profit_loss(stock, user_id) 
-    else: 
-        print("Thank you for using our service!")
+        history_for_profit_loss(stock, user_id)
+    else:
+        print("Thank you for using our Unibril Traders!")
