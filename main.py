63e677ed33d842871336b14ea192aad95c949772
Unibril Karki <unibrilkarki@gmail.com>
2026-04-06 21:20:08 +0545

    added reset password option

--- main.py
+++ main.py
@@ -1,61 +1,16 @@
-import bcrypt
 import mysql.connector
-import yfinance as yf
 import sell_stocks
 import buy_stocks
 import statement
- 
-def login(userid, password):
-    conn = mysql.connector.connect(
-        host="localhost",
-        user="root", 
-        password="",
-    )
-    cursor = conn.cursor()
-    cursor.execute("CREATE DATABASE IF NOT EXISTS USER_DETAILS")
-    cursor.execute("USE USER_DETAILS")
-    cursor.execute("""CREATE TABLE IF NOT EXISTS users (user_id INT PRIMARY KEY, username VARCHAR(255), password VARCHAR(60), balance FLOAT)""")
-    cursor.execute("SELECT password FROM users WHERE user_id = %s", (userid,))
-    result = cursor.fetchone()
-    if result:
-        stored = result[0]
-        if isinstance(stored, str):
-            stored = stored.encode()
-        if bcrypt.checkpw(password.encode(), stored):
-            print("Login successful!")
-            conn.close()
-            cursor.close()
-            return True
-    print("Invalid userid or password.")
-    conn.close()
-    cursor.close()
-    return False
-        
-
-def sign_up(name, password):
-    conn = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="" 
-    )
-    cursor = conn.cursor()
-    cursor.execute("CREATE DATABASE IF NOT EXISTS USER_DETAILS")
-    cursor.execute("USE USER_DETAILS")  
-    cursor.execute("""CREATE TABLE IF NOT EXISTS users (user_id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(255), password VARCHAR(60),balance FLOAT )""")
-    hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
-    cursor.execute("INSERT INTO users (username, password, balance) VALUES (%s, %s, %s)", (name, hashed, 100000.0))
-    conn.commit()
-    userid = cursor.lastrowid
-    print("Sign-up successful! Your user ID is:", userid)
-    cursor.close()
-    conn.close()
-    return userid
+from auth import login, sign_up, reset_password
+from databse import init_db, get_connection 
+init_db()
 
 print("Welcome to Unibril's Finance Tracker!")
 logged_in = False
 userid = None
 while not logged_in:
-    intial = input("Login or SignUp ")
+    intial = input("Login or SignUp or Resetpassword ")
     if intial.lower() =="login":
         print("Enter your userid and password to log in.")
         while True: 
@@ -80,38 +35,16 @@ while not logged_in:
         userid = sign_up(name, password)
         if userid:
             print("You can now log in with your new credentials.")
+    elif intial.lower().strip() == "resetpassword":
+        userid = int(input("Enter your userid: "))
+        username = input("Enter your username: ")
+        new_password = input("Enter new password: ")
+        reset_password(userid, username, new_password)
     else:
         print("Invalid option. Please enter 'Login' or 'SignUp'.")
 if logged_in:
     current_user_id = userid
-    conn = mysql.connector.connect(
-        host="localhost",
-        user="root",            
-        password=""
-    )
-    cursor = conn.cursor()
-    cursor.execute("CREATE DATABASE IF NOT EXISTS USER_DETAILS")
-    cursor.execute("USE USER_DETAILS")   
-    cursor.execute("""CREATE TABLE IF NOT EXISTS financial_details (
-        stock_id INT PRIMARY KEY AUTO_INCREMENT,
-        user_id INT,
-        ticker VARCHAR(10),
-        buy_price FLOAT, 
-        quantity INT, 
-        FOREIGN KEY (user_id) REFERENCES users(user_id)
-    )""")
-    cursor.execute("""CREATE TABLE IF NOT EXISTS transaction_history (
-                   transaction_id INT PRIMARY KEY AUTO_INCREMENT,
-                   user_id INT, ticker VARCHAR(10), 
-                   buy_price FLOAT, 
-                   sell_price FLOAT, 
-                   quantity INT,
-                   FOREIGN KEY (user_id) REFERENCES users(user_id))"""
-                   )
-    conn.commit()
-    cursor.close()
-    conn.close()
-
+ 
     print("You are now logged in. You can view your statement, buy stocks, or sell stocks.")
     from statement import view_statement
     from buy_stocks import buy_stonks
