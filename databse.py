cd441e55aae25ac7d875902d1a21d89943a6a615
Unibril Karki <unibrilkarki@gmail.com>
2026-04-06 21:21:04 +0545

    seperated database connection from main.py

---
+++ databse.py
@@ -0,0 +1,38 @@
+import mysql.connector
+
+def get_connection():
+    conn = mysql.connector.connect(host="localhost", user="root", password="")
+    conn.cursor().execute("USE USER_DETAILS")
+    return conn
+
+def init_db():
+    conn = mysql.connector.connect(host="localhost", user="root", password="")
+    cursor = conn.cursor()
+    cursor.execute("CREATE DATABASE IF NOT EXISTS USER_DETAILS")
+    cursor.execute("USE USER_DETAILS")
+    cursor.execute("""CREATE TABLE IF NOT EXISTS users (
+        user_id INT AUTO_INCREMENT PRIMARY KEY,
+        username VARCHAR(255),
+        password VARCHAR(60),
+        balance FLOAT
+    )""")
+    cursor.execute("""CREATE TABLE IF NOT EXISTS financial_details (
+        stock_id INT AUTO_INCREMENT PRIMARY KEY,
+        user_id INT,
+        ticker VARCHAR(10),
+        buy_price FLOAT,
+        quantity INT,
+        FOREIGN KEY (user_id) REFERENCES users(user_id)
+    )""")
+    cursor.execute("""CREATE TABLE IF NOT EXISTS transaction_history (
+        transaction_id INT AUTO_INCREMENT PRIMARY KEY,
+        user_id INT,
+        ticker VARCHAR(10),
+        buy_price FLOAT,
+        sell_price FLOAT,
+        quantity INT,
+        FOREIGN KEY (user_id) REFERENCES users(user_id)
+    )""")
+    conn.commit()
+    cursor.close()
+    conn.close()
